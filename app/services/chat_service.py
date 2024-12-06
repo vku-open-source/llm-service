6181e86ee4c5bb923e043ae6c9c4ee3a4d6cbf3b
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-12-06 09:47:34 +0700

    vietnamese for warning

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -50,6 +50,12 @@ class ChatService:
             if 'stationCode' in data:
                 del data['stationCode']
             data["Khu vực"] = data.get("label")
+            if "warning_level" in data:
+                data["Mức độ cảnh báo"] = data.get("warning_level")
+                del data["warning_level"]
+            if "warning_type" in data:
+                data["Loại cảnh báo"] = data.get("warning_type")
+                del data["warning_type"]
             
         texts.extend([f"Dữ liệu cảnh báo thiên tai ngày {datetime.now().strftime('%d/%m/%Y')} theo định dạng json: "+str(data) for data in vndms_data])
         print(texts)
